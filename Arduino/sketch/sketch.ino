babb912655ed37e1041a928f916b914145a0994f
tazlaro <77499119+tazlaro@users.noreply.github.com>
2021-01-15 16:37:26 +0100

    RFID reader + LCD

--- Arduino/sketch/sketch.ino
+++ Arduino/sketch/sketch.ino
@@ -1,26 +1,39 @@
-// RFID reader - detection of my cards
+// RFID reader + LCD - detection of my cards
 
-// libraries
+// libraries for RFID reader
 #include <SPI.h>
 #include <MFRC522.h>
+// libraries for LCD (I2C)
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
 // map pins on board (UNO R3) for SDA a RST pins on reader (for SPI communication)
 #define SDA_PIN 10
 #define RST_PIN 9
 
-// instantiate (from lib.)
+// instantiate RFID reader (from lib.)
 MFRC522 rfid(SDA_PIN, RST_PIN);
+// instantiate LCD (I2C address is 0x27)
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() {
-  // serial communication - speed: 9600 baud
+  // serial monitor, communication speed: 9600 baud
   Serial.begin(9600);
+  
   // SPI communication
   SPI.begin();
   // RFID reader initialize communication
   rfid.PCD_Init();
+  
+  // setup LCD
+  lcd.begin();
+  lcd.backlight();
+  lcd.noBlink();
+  setScreanHello();
 }
 
 void loop() {
+  
   // search for RFID tags - if not return (= again)
   if ( ! rfid.PICC_IsNewCardPresent())
     return;
@@ -31,8 +44,16 @@ void loop() {
 
   // Print info - tag version
   MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+ // char name[] = rfid.PICC_GetTypeName(piccType);
   Serial.print("Type: ");
   Serial.println(rfid.PICC_GetTypeName(piccType));
+
+//  //Show info - tag version
+//  lcd.clear();
+//  lcd.setCursor ( 0, 0 );
+//  lcd.print("Type:");
+//  lcd.setCursor ( 0, 1 );
+//  lcd.print(rfid.PICC_GetTypeName(piccType));
 
   // Check supported Types
   if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
@@ -51,6 +72,7 @@ void loop() {
   // My (known) cards
   if (rfid.uid.uidByte[0] == 0x79 & rfid.uid.uidByte[1] == 0xCA & rfid.uid.uidByte[2] == 0xD1 & rfid.uid.uidByte[3] == 0x98) {
     Serial.println("Found card #1");
+    handlingOfCards(true);
   }
   else if (rfid.uid.uidByte[0] == 0x76 & rfid.uid.uidByte[1] == 0xCF & rfid.uid.uidByte[2] == 0x60 & rfid.uid.uidByte[3] == 0x29) {
     Serial.println("Found card #2");
@@ -69,13 +91,64 @@ void loop() {
   //  }
   else {
     Serial.println("Found UNKNOWN card!");
+    handlingOfCards(false);
   }
 
-  Serial.println();
+//  Serial.println(); // moved to handlingOfCards()
 
   // end of communication
   rfid.PICC_HaltA();
   rfid.PCD_StopCrypto1();
+}
+
+void setScreanHello() {
+  lcd.clear();
+  lcd.setCursor ( 0, 0 );
+  lcd.print("-------------------");
+  lcd.setCursor ( 7, 1 );
+  lcd.print("HELLO");
+  lcd.setCursor ( 0, 2 );
+  lcd.print("Waiting for a card.");
+  lcd.setCursor ( 0, 3 );
+  lcd.print("-------------------");
+}
+void handlingOfCards(bool known){
+  if (known == true) {
+    Serial.println("Found KNOWN card");
+    Serial.println();
+    setScreanEnter();
+    delay(5000);
+  } else {
+    Serial.println("Found UNKNOWN card!");
+    Serial.println();
+    setScreanNoEnter();
+    delay(2000);
+  }
+  setScreanHello();
+}
+
+void setScreanEnter() {
+  lcd.clear();
+  lcd.setCursor ( 0, 0 );
+  lcd.print("-------------------");
+  lcd.setCursor ( 6, 1 );
+  lcd.print("WELCOME");
+  lcd.setCursor ( 3, 2 );
+  lcd.print("You may enter.");
+  lcd.setCursor ( 0, 3 );
+  lcd.print("-------------------");
+}
+
+void setScreanNoEnter() {
+  lcd.clear();
+  lcd.setCursor ( 0, 0 );
+  lcd.print("-------------------");
+  lcd.setCursor ( 3, 1 );
+  lcd.print("UNKNOWN card.");
+  lcd.setCursor ( 3, 2 );
+  lcd.print("Entry denied!");
+  lcd.setCursor ( 0, 3 );
+  lcd.print("-------------------");
 }
 
 // print of ID in hexadecimal format
